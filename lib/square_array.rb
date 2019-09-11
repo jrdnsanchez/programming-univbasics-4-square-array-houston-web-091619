c8eebc93c334f7c23b0449995541cafa43038f18
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-11 14:25:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(numbers)
-
+def square_array(array)
+  counter = 0
+  while array[counter] do
+    array[counter] = array[counter]**
+    puts array[counter]
+    counter +=1
+  end
 end
